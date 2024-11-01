335bf761cea543fb0e111251f288dec15923f064
Karina <kbrilhante@gmail.com>
2024-11-01 09:30:26 -0300

    game updated

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -2,23 +2,26 @@ let images = {
     imgs: [],
     next: "",
     theEnd: "",
+    ready: "",
 };
 
 let participants = [];
 
 let titles = [];
 
-let imgInfo = {
+let gameInfo = {
     background: 0,
     img: "",
     milli: 0,
     player: "Squeaky",
+    status: 0, // 0 = ready, 1 = next, 2 = random image
 }
 
 function preload() {
     document.getElementById("lblParticipants").innerText = random(PARTICIPANTSTITLES) + ":";
     document.getElementById("divSetup").hidden = false;
     images.next = loadImage("./ibagens/next.png");
+    images.ready = loadImage("./ibagens/ready.png");
     images.theEnd = loadImage("./ibagens/theEnd.png");
     for (let i = 1; i <= IMAGESNUMBER; i++) {
         images.imgs.push(loadImage("./ibagens/" + i + ".png"));
@@ -37,45 +40,72 @@ function setup() {
 
     imageMode(CENTER);
 
-    imgInfo.img = images.theEnd;
+    gameInfo.img = images.ready;
     noLoop();
 }
 
 function draw() {
-    background(imgInfo.background);
-    image(imgInfo.img, width / 2, height / 2, width, height);
+    background(gameInfo.background);
+    image(gameInfo.img, width / 2, height / 2, width, height);
     // write player's name on screen
     textSize(20);
     textAlign(LEFT, TOP);
-    const txt = imgInfo.player + " is " + imgInfo.action;
+    const txt = gameInfo.player + gameInfo.action;
     text(txt, 10, 10);
-    if (imgInfo.milli <= millis()) changeImg();
+    if (gameInfo.milli <= millis() && gameInfo.milli != 0) handleChange();
+}
+
+function handleChange() {
+    switch (gameInfo.status) {
+        case 0:
+            nextPlayer();
+            break;
+        case 1:
+            changeImg();
+            break;
+        case 2:
+            nextPlayer();
+            break;
+        default:
+            gameInfo.status = 0;
+            break;
+    }
 }
 
 function changeBG() {
     push();
     colorMode(HSL, 100);
-    imgInfo.background = color(random(100), 50, 60);
+    gameInfo.background = color(random(100), 50, 60);
     pop();
 }
 
 function changeImg() {
     changeBG();
-
-    imgInfo.img = random(images.imgs);
-    
-    const rndTime = random(20000, 30000);
-    imgInfo.milli = millis() + rndTime;
+    gameInfo.status = 2;
+    gameInfo.img = random(images.imgs);
+    gameInfo.milli = setTimer(random(20000, 30000));;
 }
 
 function nextPlayer() {
     changeBG();
-    imgInfo.player = participants.shift();
-    imgInfo.action = random(PARTICIPANTSACTIONS);
-    console.log(imgInfo)
-    console.log(participants)
-    imgInfo.img = images.next;
-    imgInfo.milli = millis() + random(20000, 30000);
+    gameInfo.status = 1;
+    gameInfo.player = participants.shift();
+    gameInfo.action = " is " + random(PARTICIPANTSACTIONS);
+    gameInfo.img = images.next;
+    gameInfo.milli = setTimer(random(20000, 30000));
+}
+
+function ready() {
+    changeBG();
+    gameInfo.status = 0;
+    gameInfo.player = "";
+    gameInfo.action = "";
+    gameInfo.img = images.ready;
+    gameInfo.milli = setTimer(5000);
+}
+
+function setTimer(time) {
+    return millis() + time;
 }
 
 function setListeners() {
@@ -84,7 +114,7 @@ function setListeners() {
 
 function begin() {
     participants = getParticipants();
-    nextPlayer();
+    ready();
     document.getElementById("title").innerText = random(titles);
     document.getElementById("divGame").hidden = false;
     document.getElementById("divSetup").hidden = true;
