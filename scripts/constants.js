9dbb86021a997827314e170072fbfd3e1343a367
Karina <58565453+kbrilhante@users.noreply.github.com>
2025-08-28 14:15:33 -0300

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,10 +1,10 @@
 const IMAGES_NUMBER = 78;
-const IMAGES_FOLDER = "/ibagens/";
-const IMAGE_READY = "/ibagens/ready.png";
-const IMAGE_NEXT = "/ibagens/next.png";
-const IMAGE_THE_END = "/ibagens/theEnd.png";
+const IMAGES_FOLDER = "./ibagens/";
+const IMAGE_READY = "./ibagens/ready.png";
+const IMAGE_NEXT = "./ibagens/next.png";
+const IMAGE_THE_END = "./ibagens/theEnd.png";
 
-const STORY_TITLES_URL = "/data/English/story_titles.txt";
-const PARTICIPANTS_ACTIONS_URL = "/data/English/participants_actions.txt";
-const PARTICIPANTS_TITLES_URL = "/data/English/participants_titles.txt";
+const STORY_TITLES_URL = "./data/English/story_titles.txt";
+const PARTICIPANTS_ACTIONS_URL = "./data/English/participants_actions.txt";
+const PARTICIPANTS_TITLES_URL = "./data/English/participants_titles.txt";
 
